83e518d6623679803ce949d97bad1205e162ec54
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-11-04 13:21:57 +0100

    add others test about email format/ email unique without errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+#le champs 'name' doit etre non-nil, ne doit pas etre vide, longueur max 50 car
+    validates :name, presence: true, length: { maximum: 50 }
+#stock dans une var le regex type 'format d'email valide'
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#le champs 'email' doit etre non-nil, ne doit pas etre vide , longueur max 255 car,
+# et conforme a la var 'VALID_EMAIL_REGEX'
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }, 
+                      uniqueness: true
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,7 +14,8 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-#verifie la validité si le champs name est remplis d'espace
+#verifie la validité si le champs name est remplis d'espace 
+#controller ->validates :name, presence: true
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid? 
@@ -22,10 +23,50 @@ class UserTest < ActiveSupport::TestCase
   end
 
 #verifie la validité si le champs email est remplis d'espace
-  test "name should be present" do
+  test "email should be present" do
     @user.email = "     "
     assert_not @user.valid? 
     puts "champ 'email' cant be empty: CHECK!"
+  end
+#verifie si name a un nbr de caractere reglementaire
+#controller ->length: { maximum: 50 }
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+    puts "name has reglementary length: CHECK!"
+  end
+#verifie si email a un nbr de caractere reglementaire / controller ->length: { maximum: 255 }
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+    puts "email has reglementary length: CHECK!"
+  end
+#test la validité du format sur un array d'email valide [regex dans controller]
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+    puts "email has right format: CHECK!"
+  end
+#test la validité du format sur un array d'email invalide
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+    puts "email has wrong format: CHECK!"
+  end
+#verifie que l'adresse saisie est unique
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+    puts "email is unique: CHECK!"
   end
 
 end
