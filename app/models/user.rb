109a9fca639cab5a2ccf29d325c86c6f5a0f0048
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-11-04 14:07:47 +0100

    crea session_controller + add crypting gem in the gemfile + add some routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
 #le champs 'name' doit etre non-nil, ne doit pas etre vide, longueur max 50 car
     validates :name, presence: true, length: { maximum: 50 }
 #stock dans une var le regex type 'format d'email valide'
@@ -7,6 +8,10 @@ class User < ApplicationRecord
 # et conforme a la var 'VALID_EMAIL_REGEX'
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX }, 
-                      uniqueness: true
+                      uniqueness: { case_sensitive: false}
+# cryptage du password
+    has_secure_password
+# format standard du password
+    validates :password, presence: true, length: { minimum: 6 }
   
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 #crée un user fictif pour exercer les tests sur lui
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 #verifie si le champs email est non-nil
   test "should be valid" do
@@ -64,9 +64,30 @@ class UserTest < ActiveSupport::TestCase
 #verifie que l'adresse saisie est unique
   test "email addresses should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-    puts "email is unique: CHECK!"
+    puts "email is unique and not case_sensitive: CHECK!"
+  end
+#verifie que l'adresse email est sauvegardée en lowcase
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+    puts "email is saved as lower-case: CHECK!"
+  end
+#verifie que le champ 'password' est non-nil
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+    puts "password is non-nil: CHECK!"
+  end
+#verifie que le password est assez long (minimum 5 car)
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+    puts "password is long enough: CHECK!"
   end
 
 end
